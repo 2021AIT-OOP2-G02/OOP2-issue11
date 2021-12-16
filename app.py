4b14f037e0917f6e91d6d824708aa69f13f4e8a5
高野晃汰 <82937684+KotaTakano@users.noreply.github.com>
2021-12-16 16:11:27 +0900

    各種調整
    いらん部分を削除

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template
-import os
 import upload
 import list
 from werkzeug.utils import secure_filename
@@ -15,21 +14,17 @@ UPLOAD_FOLDER = BASE_DIR / "img" / "upload_img"
 def index():
     return render_template("index.html")
 
-#GETの処理
-@app.route('/up/', methods=['GET'])
-def up_get():
-    return render_template('index.html', message = '画像を選択しよう', flag = False)
+
 
 #POSTの処理
 @app.route('/up/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files["the_file"]
-        #任意の階層を相対パスで指定
-        f.save(UPLOAD_FOLDER / secure_filename(f.filename))
-        #アップロードしてサーバーにファイルが保存されたらfinishedを表示
+        if ("file" in request.files): #存在確認
+            f = request.files["the_file"]
+            #任意の階層を相対パスで指定
+            f.save(UPLOAD_FOLDER / secure_filename(f.filename))
         
-    
     return render_template('index.html')
 
 if __name__ == "__main__":
