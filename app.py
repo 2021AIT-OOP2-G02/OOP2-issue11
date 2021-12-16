4388c8093262720db4ab686e59bac6b7229dd19f
高野晃汰 <82937684+KotaTakano@users.noreply.github.com>
2021-12-16 15:52:52 +0900

    画像アップロードプログラム完成
    画像アップロードのシステムのみが完成、相対パスで保存先ファイルを設定したので環境が変わっても問題ないはず

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import os
 import upload
 import list
 from werkzeug.utils import secure_filename
+from pathlib import Path
+
 
 app = Flask(__name__)
+
+BASE_DIR = Path(__file__).resolve().parent
+UPLOAD_FOLDER = BASE_DIR / "img" / "upload_img"
 
 @app.route('/')
 def index():
@@ -20,10 +25,13 @@ def up_get():
 def upload_file():
     if request.method == 'POST':
         f = request.files["the_file"]
-        #任意の階層をフルパスで指定(macの場合。任意のユーザー名は変更してください。)
-        f.save('/Users/任意のユーザー名/Python/myproject/uploads/' + secure_filename(f.filename))
+        #任意の階層を相対パスで指定
+        f.save(UPLOAD_FOLDER / secure_filename(f.filename))
         #アップロードしてサーバーにファイルが保存されたらfinishedを表示
-        return render_template('finished.html')
-    else:
-    	#GETでアクセスされた時、uploadsを表示
-    	return render_template('uploads.html')
+        
+    
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # debugモードが不要の場合は、debug=Trueを消してください
+    app.run(debug=True)
