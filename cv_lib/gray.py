e80bbfebc68d193cc900b2127c9c024cd1ac7978
わだたける <wdtkru@icloud.com>
2021-12-18 23:57:51 +0900

    一連の動きを連結させた
    一連の動きとは
    watch開始→画像アップロード→watchdog監視で追加検出→フィルタをかける→リストに表示

--- cv_lib/gray.py
+++ cv_lib/gray.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
-import cv2
+import cv2, os
+
+def gray(path):
+    #画像読み込み
+    img = cv2.imread(path)
+
+    #読み込んだ画像をグレースケール化
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('./static/img/gray_img/gray_'+os.path.basename(path), img_gray)
+    # ↑引数として持ってきたfileの名前にgray_を足してファイル名としてる
+
+    #確認用出力
+    #cv2.imshow('color', img_gray)
+    #cv2.waitKey(0)
 
 
-#画像読み込み
-img = cv2.imread('img/upload_img/test.png')
+    #グレースケールされた画像を二値化
+    ret2, img_threshold = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU)
+    cv2.imwrite('./static/img/gray_img/bin_'+os.path.basename(path), img_threshold)
 
-#読み込んだ画像をグレースケール化
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('img/gray_img/gray.jpg', img_gray)
-
-#確認用出力
-#cv2.imshow('color', img_gray)
-#cv2.waitKey(0)
-
-
-#グレースケールされた画像を二値化
-ret2, img_threshold = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU)
-cv2.imwrite('img/threshold_img/threshold.jpg', img_threshold)
-
-#確認用出力
-#cv2.imshow('color', img_threshold)
-#cv2.waitKey(0)
+    #確認用出力
+    #cv2.imshow('color', img_threshold)
+    #cv2.waitKey(0)`
--- cv_lib/main.py
+++ cv_lib/main.py
@@ -1,2 +1,9 @@
-def start_opencv():
+from . import canny
+from . import gray
+
+def start_opencv(path):
     print("start_opencv...")
+
+    # それぞれの画像処理を実行
+    canny.canny(path)
+    gray.gray(path)
